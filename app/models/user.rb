7b5205d01cbe06e5e2d1dca21e3fd0dfa736ba99
kristinetlam <kristinetlam21@gmail.com>
2024-09-21 18:44:57 -0500

    update profile avatar library frontend working, need to add backend now

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,4 @@ class User < ApplicationRecord
   def self.from_google(email:, full_name:, uid:, avatar_url:)
     create_with(uid: uid, full_name: full_name, avatar_url: avatar_url).find_or_create_by!(email: email)
   end
-
-
 end
