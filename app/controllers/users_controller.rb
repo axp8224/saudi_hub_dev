08e75a67fcedd2f510784e939259db626ad496bb
kristinetlam <kristinetlam21@gmail.com>
2024-09-21 18:23:49 -0500

    added edit form and working on update functionality now

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,31 @@
 class UsersController < ApplicationController
     before_action :authenticate_user!
+    before_action :set_user, only: %i[edit update]
     
     def show
       @user = current_user
     end
+
+    def edit
+    end
+
+    def update
+      if @user.update(user_params)
+        redirect_to root_path, notice: 'Profile updated successfully!'
+      else
+        render :edit
+      end
+    end
+
+    private 
+
+    def set_user
+      @user = current_user
+    end
+  
+    def user_params
+      params.require(:user).permit(:full_name, :email, :avatar)
+    end
+
   end
   
