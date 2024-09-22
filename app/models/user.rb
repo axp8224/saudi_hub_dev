b23223f38f17990e91ce3b0e18239f661dbadafb
kristinetlam <kristinetlam21@gmail.com>
2024-09-22 17:26:48 -0500

    added pfp file validation and arabic localization

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,24 @@ class User < ApplicationRecord
 
   belongs_to :major, optional: true
 
+  validate :acceptable_avatar
+
   def self.from_google(email:, full_name:, uid:, avatar_url:)
     create_with(uid: uid, full_name: full_name, avatar_url: avatar_url, major_id: Major.find_by(name: "").try(:id)).find_or_create_by!(email: email)
   end
+
+  private
+  def acceptable_avatar
+    return unless avatar.attached?
+
+    allowed_types = ['image/jpeg', 'image/png']
+    unless allowed_types.include?(avatar.content_type)
+      errors.add(:avatar, I18n.t('users.edit.error_messages.file_type_error'))
+    end
+
+    # if avatar.byte_size > 250.kilobytes
+    #   errors.add(:avatar, I18n.t('users.edit.error_messages.file_size_too_large'))
+    # end
+  end
+
 end
