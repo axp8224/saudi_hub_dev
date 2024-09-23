6f0ed28fdf3f6e8fb6aa8531d24246aa3a0b59fe
Aryan <axp8224@tamu.edu>
2024-09-23 18:13:01 -0500

    fix auth logic

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,5 @@
 class ApplicationController < ActionController::Base
   before_action :set_locale
-  before_action :authenticate_user!
 
   def set_locale
     I18n.locale = cookies[:locale] || I18n.default_locale
