ea630be5fcc08f5113c591f0484508172ea47b92
root <root@localhost>
2024-09-23 22:43:15 +0000

    fix route authentication

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,7 +3,7 @@ class UsersController < ApplicationController
     before_action :set_user, only: %i[edit update]
     before_action :set_majors, only: %i[edit update]
     before_action :set_classifications, only: %i[edit update]
-    
+
     def show
       @user = current_user
     end
