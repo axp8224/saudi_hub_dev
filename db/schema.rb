2e25ec7dc4981868d4e4de2bff640af0e12ff6a3
kristinetlam <kristinetlam21@gmail.com>
2024-09-23 15:38:41 -0500

    resolved conflicts from test

--- db/schema.rb
+++ db/schema.rb
@@ -62,6 +62,14 @@ ActiveRecord::Schema[7.0].define(version: 2024_09_21_214514) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "roles", force: :cascade do |t|
+    t.string "name", null: false
+    t.boolean "can_moderate", default: false
+    t.boolean "can_promote", default: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email", null: false
     t.string "full_name"
@@ -73,10 +81,12 @@ ActiveRecord::Schema[7.0].define(version: 2024_09_21_214514) do
     t.bigint "major_id", default: 1
     t.bigint "class_year_id", default: 1
     t.string "bio"
+    t.bigint "role_id", default: 1
     t.index ["class_year_id"], name: "index_users_on_class_year_id"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["role_id"], name: "index_users_on_role_id"
     t.index ["major_id"], name: "index_users_on_major_id"
+    t.index ["role_id"], name: "index_users_on_role_id"
   end
 
   add_foreign_key "users", "roles"
@@ -84,4 +94,5 @@ ActiveRecord::Schema[7.0].define(version: 2024_09_21_214514) do
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
   add_foreign_key "users", "class_years"
   add_foreign_key "users", "majors"
+  add_foreign_key "users", "roles"
 end
