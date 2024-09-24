aaec0d6ac6e372d45f0c8fe0f4c31e1ed1558b6a
Josiah Castillo <josiahcastillo03@yahoo.com>
2024-09-24 04:09:08 +0000

    manage user # per page selector

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -4,12 +4,14 @@ module Admin
     before_action :admin_only
 
     def index
+      per_page = params[:per_page] || 10  # Default to 10 if not set
       if params[:search].present?
-        @users = User.where("full_name ILIKE ?", "%#{params[:search]}%").page(params[:page]).per(10)
+        search_query = "%#{params[:search]}%"
+        @users = User.where("full_name ILIKE :search OR email ILIKE :search", search: search_query).page(params[:page]).per(per_page)
       else
-        @users = User.page(params[:page]).per(10)  # 10 users per page
+        @users = User.page(params[:page]).per(per_page)
       end
-    end
+    end       
 
     def edit
       @user = User.find(params[:id])
