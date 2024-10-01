246440990075a3dc4f8e7bef876918ff60e91cfe
Brigham Pettit <bpettit2003@gmail.com>
2024-10-01 13:57:07 +0000

    unifinished: unit test for resource

---
+++ spec/models/resource_spec.rb
@@ -0,0 +1,9 @@
+require 'rails_helper'
+
+RSpec.describe Resource, type: :model do
+  pending "add some examples to (or delete) #{__FILE__}"
+end
+
+# valid with all required fields
+# invalid without any required field
+# status defaults to "pending" for new resources
