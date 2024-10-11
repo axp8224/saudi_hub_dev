6fd86ebef828c83be4d7d6f4ed69bcf89f73361a
Josiah Castillo <josiahcastillo03@yahoo.com>
2024-10-11 04:13:35 +0000

    all tests pass

--- spec/features/admin/resource_admin_spec.rb
+++ spec/features/admin/resource_admin_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
-RSpec.feature "Resources", type: :feature do
+RSpec.feature 'Resources', type: :feature do
   let(:admin) { User.find_by(email: 'admin@example.com') }
   let(:restaurant_type) { ResourceType.find_by(title: 'Restaurants') }
   let(:apartment_type) { ResourceType.find_by(title: 'Apartments') }
@@ -12,18 +12,19 @@ RSpec.feature "Resources", type: :feature do
   before do
     omniauth_mock_auth_hash_ADMIN
     visit new_user_session_path
-    click_button "Log in with Google"
+    click_button 'Log in with Google'
+    click_button 'I Accept'
   end
 
-  scenario "Admin sees all pending resources for approval" do 
+  scenario 'Admin sees all pending resources for approval' do
     # can filter resources on status
     visit admin_resources_path
 
-    expect(page).to have_content("Resource Manager")
+    expect(page).to have_content('Resource Manager')
 
     select t('admin.resources.status_pending'), from: 'status'
 
-    click_on "Search"
+    click_on 'Search'
 
     pending_resources.each do |resource|
       expect(page).to have_content(resource.title)
@@ -33,12 +34,12 @@ RSpec.feature "Resources", type: :feature do
     end
   end
 
-  scenario "Admin can edit resources" do
+  scenario 'Admin can edit resources' do
     # can technically edit any resource as an admin, but here we do pending because it is the most common likely use case.
     visit admin_resources_path
-    
+
     select t('admin.resources.status_pending'), from: 'status'
-    click_on "Search"
+    click_on 'Search'
 
     pending_resource = pending_resources.first
     original_description = pending_resource.description
@@ -50,23 +51,23 @@ RSpec.feature "Resources", type: :feature do
     expect(page).to have_content(original_description) # ensure this is the page for the first pending resource
 
     new_description = 'Updated sample resource description.'
-    fill_in 'Description', with: new_description 
+    fill_in 'Description', with: new_description
 
     click_button 'Save Resource'
 
     expect(page).to have_current_path(admin_resources_path) # redirect back to manage resources page
 
     select t('admin.resources.status_pending'), from: 'status'
-    click_on "Search"
+    click_on 'Search'
 
     expect(page).not_to have_content(original_description)
     expect(page).to have_content(new_description)
   end
 
-  scenario "Admin can approve resources" do
+  scenario 'Admin can approve resources' do
     # edit status of a resource from pending to active
     visit admin_resources_path
-    
+
     select t('admin.resources.status_pending'), from: 'status'
 
     pending_resources = Resource.where(status: 'pending')
@@ -87,22 +88,22 @@ RSpec.feature "Resources", type: :feature do
     expect(page).to have_current_path(admin_resources_path) # redirect back to manage resources page
 
     select t('admin.resources.status_pending'), from: 'status'
-    click_on "Search"
+    click_on 'Search'
 
     expect(page).not_to have_content(original_description)
 
     select t('admin.resources.status_active'), from: 'status'
-    click_on "Search"
+    click_on 'Search'
 
     expect(page).to have_content(original_description)
   end
 
-  scenario "Admin can reject resources" do
+  scenario 'Admin can reject resources' do
     # edit status of a resource from pending to archived
     visit admin_resources_path
-    
+
     select t('admin.resources.status_pending'), from: 'status'
-    click_on "Search"
+    click_on 'Search'
 
     original_description = pending_resources.first.description
 
@@ -119,14 +120,13 @@ RSpec.feature "Resources", type: :feature do
     expect(page).to have_current_path(admin_resources_path) # redirect back to manage resources page
 
     select t('admin.resources.status_pending'), from: 'status'
-    click_on "Search"
+    click_on 'Search'
 
     expect(page).not_to have_content(original_description)
 
     select t('admin.resources.status_archived'), from: 'status'
-    click_on "Search"
+    click_on 'Search'
 
     expect(page).to have_content(original_description)
   end
-  
 end
