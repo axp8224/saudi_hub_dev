0b0dc2bc742af25c39b1e489aa139dff209e8e30
Aryan <123004220+axp8224@users.noreply.github.com>
2024-10-12 15:46:54 -0500

    WIP on user_profile

--- app/controllers/resources_controller.rb
+++ app/controllers/resources_controller.rb
@@ -8,6 +8,30 @@ class ResourcesController < ApplicationController
     @resources = @resources.where(resource_type_id: params[:resource_type_id])
   end
 
+  def new
+    @resource = Resource.new
+    @resource_types = ResourceType.all
+  end
+
+  def create
+    @resource = Resource.new(resource_params)
+    @resource.author = current_user
+    @resource.status = 'pending' # Set status to pending
+
+    if @resource.save
+      redirect_to resources_path, notice: 'Resource was successfully created.'
+    else
+      @resource_types = ResourceType.all
+      render :new
+    end
+  end
+
+  private
+
+  def resource_params
+    params.require(:resource).permit(:title, :description, :resource_type_id, images: [])
+  end
+
   def show
     @resource = Resource.find(params[:id])
     @resource_author = User.find(@resource.user_id) if @resource.user_id.present?
--- app/models/resource.rb
+++ app/models/resource.rb
@@ -1,6 +1,7 @@
 class Resource < ApplicationRecord
   belongs_to :resource_type
   belongs_to :author, class_name: 'User', foreign_key: 'user_id'
+  has_many_attached :images
 
   validates :title, presence: true
   validates :description, presence: true
