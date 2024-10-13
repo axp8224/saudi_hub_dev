32a331f09f4c1bedf8cfde18a1e67fcee88b53e7
Brigham Pettit <bpettit2003@gmail.com>
2024-10-13 20:26:16 +0000

    Added help page

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,8 @@
+class PagesController < ApplicationController
+  # responsible for static pages
+  def home
+  end
+
+  def help 
+  end
+end
--- spec/features/documentation/help_page_spec.rb
+++ spec/features/documentation/help_page_spec.rb
@@ -1,18 +1,11 @@
 require 'rails_helper'
 
 RSpec.feature "HelpPage", type: :feature do
-  
-  before do
-    omniauth_mock_auth_hash
-    visit new_user_session_path
-    click_button "Log in with Google"
-    click_button "I Accept"
-  end
 
   scenario "User accesses the help page" do
-    visit help_path()
+    visit help_path
 
-    expect(page).to have_content("How to Use This Website")
+    expect(page).to have_content(t('pages.help.title'))
   end
 
 end
