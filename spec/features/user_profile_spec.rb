b219e96ad111cde1c201e94868a3e4650997e8b0
root <root@localhost>
2024-10-24 19:25:35 +0000

    fixup! Add tests for view profile

--- spec/features/user_profile_spec.rb
+++ spec/features/user_profile_spec.rb
@@ -67,6 +67,32 @@ RSpec.feature "UserProfile", type: :feature do
     expect(user.major.name).to eq 'Accounting'
   end
 
+  scenario "Profile update fails" do 
+    visit edit_user_profile_path(user)
+
+    expect(page).to have_field('Full Name', with: user.full_name)
+    expect(page).to have_selector("input[type='email'][readonly][value='#{user.email}']")    
+    expect(page).to have_field('Graduation Year', with: user.grad_year.to_s)
+    expect(page).to have_field('About Me', with: user.bio)
+    
+    fill_in 'Full Name', with: 'Updated Name'
+    fill_in 'Graduation Year', with: '2023'
+    fill_in 'About Me', with: 'Updated bio here.'
+
+    save_and_open_page
+    expect(page).to have_select('user_class_year_id', visible: true)
+    select 'Junior', from: 'user_class_year_id' 
+
+    expect(page).to have_select('user_major_id', visible: true)
+    select 'Accounting', from: 'user_major_id' 
+
+    allow_any_instance_of(User).to receive(:update).and_return(false)
+
+    click_button 'Update Profile'
+
+    expect(page).to have_content(t('flash.profile.update_failure'))
+  end
+
   scenario "User views some else's profile" do 
     visit users_path 
 
@@ -87,10 +113,13 @@ RSpec.feature "UserProfile", type: :feature do
 
   scenario "User tries to edit someone else's profile" do 
     other_user = User.find_by(email: "sample2@example.com")
-
+    puts "link: #{edit_user_profile_path(other_user)}"
     visit edit_user_profile_path(other_user)
+    # because of how our routes and controllers are defined, the user parameter does nothing :)
 
-    expect(page).to have_content(t('flash.profile.edit.only_your_profile')) 
+    expect(page).not_to have_content(other_user.full_name)        # not allowed to edit other user
+    expect(page).to have_selector("input[type='email'][readonly][value='#{user.email}']")  # this is edit current user
   end
+
 
 end
