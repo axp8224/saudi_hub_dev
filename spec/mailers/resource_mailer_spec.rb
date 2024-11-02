62a638760e87dce68b994bbfc162435bd807d103
Dongha Cho <donghatamu@tamu.edu>
2024-11-02 17:23:16 -0500

    test written

--- spec/mailers/resource_mailer_spec.rb
+++ spec/mailers/resource_mailer_spec.rb
@@ -1,5 +1,33 @@
 require "rails_helper"
 
 RSpec.describe ResourceMailer, type: :mailer do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:resource) { create(:resource, feedback: "Great job!", user: create(:user)) }
+
+  describe 'approval_notification' do
+    let(:mail) { ResourceMailer.approval_notification(resource) }
+
+    it 'renders the headers' do
+      expect(mail.subject).to eq("Your Resource Submission Has Been Approved")
+      expect(mail.to).to eq([resource.user.email])
+      expect(mail.from).to eq(['no-reply@example.com'])
+    end
+
+    it 'renders the body with feedback' do
+      expect(mail.body.encoded).to match("Great job!")
+    end
+  end
+
+  describe 'rejection_notification' do
+    let(:mail) { ResourceMailer.rejection_notification(resource) }
+
+    it 'renders the headers' do
+      expect(mail.subject).to eq("Your Resource Submission Has Been Rejected")
+      expect(mail.to).to eq([resource.user.email])
+      expect(mail.from).to eq(['no-reply@example.com'])
+    end
+
+    it 'renders the body with feedback' do
+      expect(mail.body.encoded).to match("Great job!")
+    end
+  end
 end
