f8dd79575b99332614761a8f8fc3430926551f73
Dongha Cho <donghatamu@tamu.edu>
2024-11-02 17:16:28 -0500

    resource mailer implemented

--- app/mailers/resource_mailer.rb
+++ app/mailers/resource_mailer.rb
@@ -1,2 +1,20 @@
 class ResourceMailer < ApplicationMailer
-end
+    default from: 'no-reply@example.com'
+  
+    def approval_notification(resource)
+      @resource = resource
+      @user = resource.user
+      @feedback = resource.feedback.presence || "No feedback was given."
+  
+      mail(to: @user.email, subject: "Your Resource Submission Has Been Approved")
+    end
+  
+    def rejection_notification(resource)
+      @resource = resource
+      @user = resource.user
+      @feedback = resource.feedback.presence || "No feedback was given."
+  
+      mail(to: @user.email, subject: "Your Resource Submission Has Been Rejected")
+    end
+  end
+  
